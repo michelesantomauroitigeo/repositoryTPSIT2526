d8496d1355d3e4f55b3fe911877bb8edcd7300a3
cristianricchiuto23 <cristian.ricchiuto23@gmail.com>
2026-02-14 14:29:52 +0100

    Add files via upload

---
+++ RICCHIUTO CRISTIAN/01_ricchiutoCristian.cpp.cpp
@@ -0,0 +1,16 @@
+//Ricchiuto Cristian
+
+#include<stdio.h>
+int main(){
+	int indirizzoBase, numeroIndirizziLogici, indirizzoFisico, i;
+	i=0;
+	printf("Inserisci indirizzo base:");
+	scanf("%d",& indirizzoBase);
+	printf("inserisci numeroIndirizziLogici:");
+	scanf("%d",& numeroIndirizziLogici);
+	for (i=1;i<=numeroIndirizziLogici;i++){
+	indirizzoFisico= indirizzoBase+numeroIndirizziLogici;
+	printf("L'indirizzo fisico e':%d",indirizzoFisico);
+}
+return 0;
+}
